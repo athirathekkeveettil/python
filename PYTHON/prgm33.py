b3a213870a45483b5ba6ee4664f3cf407c844b92
Athira Thekkeveettil <147636246+athirathekkeveettil@users.noreply.github.com>
2023-11-07 21:27:54 +0530

    Add files via upload

--- PYTHON/prgm33.py
+++ PYTHON/prgm33.py
@@ -1,8 +1,11 @@
-s=input("Enter a string:");
-if len(s)==2:
-    res=s*2
-elif len(s)>2:
-    res=s[:2]+s[-2:]
+# generate a string by combining 1st 2 characters and last 2 characters from an input string.If length of input strings is 2,then resultant string must be a concatenation of those characters or if length is less than than 2,return an empty string instead
+
+a=input("Enter a string:");
+if len(a)==2:
+    b=a*2
+elif len(a)>2:
+    b=a[:2]+a[-2:]
+    print(b)
 else:
-    print("empty")
-print(res)
+    print("Empty")
+    
